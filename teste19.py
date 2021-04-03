d176d4d33edc77c1fe5b6e8bcc77a3d8113309ba
iagor <iagorluchesi@gmail.com>
2021-04-02 22:56:56 -0400

    commit do 19 de novo

--- teste19.py
+++ teste19.py
@@ -1,5 +1,6 @@
 nome = input(str('digite seu nome :'))
 print(len(nome.strip())) #para usar o lower() ou outro do tipo precisa usar a: variável.lower().find(a) // isso foi so um examplo 
-print(nome.lower().split())
-#n finalizado
+dividido = nome.split()
+print(nome.lower().upper())
+print(dividido[0])
 
