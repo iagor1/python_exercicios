08dbd278fa6730c18c4f6623c22820877e062c51
iagor M <iagorluchesi@gmail.com>
2021-05-04 00:57:59 -0400

    alguns commits

---
+++ teste39.py
@@ -0,0 +1,6 @@
+soma = 0 
+for c in range(0, 500, 3):
+    if c % 3 == 0:
+         print(c)
+        # soma = soma +c
+        # print(soma)
---
+++ teste40.py
@@ -0,0 +1,5 @@
+for c in range(0,6):
+    n = int(input('digite um valor : '))
+    if n % 2 == 0 :
+         soma = n + n
+         print(soma)
