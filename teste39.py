fff699beae32a0208b5c21a30ecf1ed73ac4ed2a
Iagor Molino Luchesi <iagorluchesi@gmail.com>
2021-05-07 22:58:43 -0400

    commit do 39

--- teste39.py
+++ teste39.py
@@ -1,6 +1,7 @@
 soma = 0 
-for c in range(0, 500, 3):
+cont = 0 
+for c in range(1, 501, 2):
     if c % 3 == 0:
-         print(c)
-        # soma = soma +c
-        # print(soma)
+         cont = cont +1 
+         soma = soma +c
+         print('a soma de todos é ', soma ,'num divisíveis por três ', c,', números somados', cont)
