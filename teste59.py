71bb8cd71ebaab3b0d752e9a0bdc814d713c9991
Iagor Molino Luchesi <iagorluchesi@gmail.com>
2021-05-19 00:00:35 -0400

    commit do 59

--- teste59.py
+++ teste59.py
@@ -2,12 +2,20 @@ import random
 contVitorias=0
 while True:
     sorteadoPc = random.randint(0,10)
-    r = str(input('Par ou ímpar  [p/i] : ')).lower().split()[0]
+    r = ' '
+    while r not in 'parimpar':  #recurso pra só digitar par ou impar 
+        r = str(input('Par ou ímpar  [p/i] : ')).lower().split()[0]
     n = int(input('digite um número : '))
     soma = n + sorteadoPc
-    if soma %2 ==0 and r == 'p':
+    if soma % 2 == 0 and r == 'p' : #par
         print('você ganhou !!')
         contVitorias+=1
         print(f'voce jogou {n} o computador jogou {sorteadoPc} a soma deles é {soma} logo é par.')
-
-print(f'voce jogou {n} o computador jogou {sorteadoPc} a soma deles é {soma} ')
+    elif soma % 2 != 0 and r== 'i' : 
+        print('você ganhou!!') #impar
+        print(f'voce jogou {n} o computador jogou {sorteadoPc} a soma deles é {soma} logo é ímpar.')
+        contVitorias+=1
+    else:
+        print('Você perdeu :( ')
+        print(f'voce jogou {n} o computador jogou {sorteadoPc} a soma deles é {soma}. Você acumulou {contVitorias} vitórias.')
+        break    
